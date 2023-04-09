c5ffcaeedeab7ebd20408d719d123ce303511454
Michael Skiles <mjrskiles@gmail.com>
2023-04-09 17:21:19 -0500

    Set test melody to zelda chest theme.

--- test/command_test_melody.sh
+++ test/command_test_melody.sh
@@ -8,24 +8,39 @@ note_off() {
     mosquitto_pub -t "toy/synth/test/command" -m "note_off"
 }
 
-quarter_len=0.25
-half_len=0.5
-whole_len=1.0
+quarter_len=0.1
+half_len=0.2
+whole_len=0.4
 
-source ../../venv/bin/activate
-(python ../../src/main.py)&
+source ../venv/bin/activate
+(python ../src/main.py)&
 pid=$!
 
 sleep 1
-freq=220.0
+freq=739.99
 note_on
-sleep $half_len
-freq=440.0
+sleep $quarter_len
+freq=698.46
 note_on
-sleep $half_len
-freq=220.0
+sleep $quarter_len
+freq=587.33
 note_on
-sleep $half_len
+sleep $quarter_len
+freq=415.30
+note_on
+sleep $quarter_len
+freq=392.00
+note_on
+sleep $quarter_len
+freq=622.25
+note_on
+sleep $quarter_len
+freq=783.99
+note_on
+sleep $quarter_len
+freq=987.77
+note_on
+sleep $quarter_len
 note_off
 
 echo "Killing $!"
