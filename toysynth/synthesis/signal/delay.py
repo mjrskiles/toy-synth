Fixed tri wave and delay normalization

toysynth/synthesis/signal/delay.py
@@ -25,8 +25,6 @@ class Delay(Component):
     def __next__(self):
         (mix, props) = next(self.signal_iter)
         amp = props["amp"]
-
-        # self.log.debug(f"mix1: {mix}")
         
         # Add the delayed signal to the mix
         if self.delay_time > 0:
@@ -35,18 +33,19 @@ class Delay(Component):
                 self.log.debug(f"Had to wrap around the delay buffer")
                 delayed_signal = np.concatenate((delayed_signal, self.delay_buffer[self.delay_time_start_index: self.delay_time_start_index + self.frames_per_chunk - len(delayed_signal)]))
             
-            # self.log.debug(f"delayed_signal: {delayed_signal}")
             delayed_signal *= self.wet_gain
+            amp += self.wet_gain
             mix += delayed_signal
 
-        # self.log.debug(f"mix2: {mix}")
-        # TODO make sure the signal is between -1 and 1
+        # make sure the signal is between -1 and 1
+        if amp > 1.0:
+            # self.log.debug(f"Normalizing signal with amp {amp}")
+            mix = mix / amp
+            amp = 1.0
 
         # Add the current signal to the delay buffer
         self.delay_buffer = np.roll(self.delay_buffer, -self.frames_per_chunk)
         self.delay_buffer[self.delay_frames - self.frames_per_chunk: self.delay_frames] = mix
-
-        # self.log.debug(f"self.delay_buffer: {self.delay_buffer}")
 
         # Update the amplitude
         self._props["amp"] = amp

toysynth/synthesis/signal/triangle_wave_oscillator.py
@@ -2,38 +2,19 @@ import logging
 import numpy as np
 
 from .oscillator import Oscillator
+from .sawtooth_wave_oscillator import SawtoothWaveOscillator
 
-class TriangleWaveOscillator(Oscillator):
+class TriangleWaveOscillator(SawtoothWaveOscillator):
     def __init__(self, sample_rate, frames_per_chunk, name="TriWaveOscillator"):
         super().__init__(sample_rate, frames_per_chunk, name=name)
     
     def __iter__(self):
-        self._wave = np.zeros(self.frames_per_chunk)
-        self._chunk_duration = self.frames_per_chunk / self.sample_rate
-        self._chunk_start_time = 0.0
-        self._chunk_end_time = self._chunk_duration
-        return self
+        return super().__iter__()
 
     def __next__(self):
-        if not self.active or self.frequency <= 0.0:
-            if self.frequency < 0.0:
-                self.log.error("Overriding negative frequency to 0")
-            self._wave = np.zeros(self.frames_per_chunk, dtype=np.float32)
-            self._props["amp"] = 0.0
-
-        else:
-            self._props["amp"] = 1.0
-            p = 1/self.frequency #calculate the period for the formula
-            ts = np.linspace(self._chunk_start_time, self._chunk_end_time, self.frames_per_chunk, endpoint=False)
-            
-            self._wave = 4 * self.amplitude / p * np.abs(((ts - (p + 4 * self.phase) / 4) % p) - p / 2) - self.amplitude
-
-            # Update the state variables for next time
-            self._chunk_start_time = self._chunk_end_time
-            self._chunk_end_time += self._chunk_duration
-
-        # Return the wave array and properties outside the else block
-        return (self._wave.astype(np.float32), self._props)
+        (sawtooth, props) = super().__next__()
+        triangle = (abs(sawtooth) - 0.5) * 2
+        return (triangle, props)
 
     def __deepcopy__(self, memo):
         return TriangleWaveOscillator(self.sample_rate, self.frames_per_chunk, name="TriWaveOscillator")